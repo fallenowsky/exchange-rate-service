08ab4040bd0b460e7f9f60d78781314680904e5d
fallenowsky <marcin.miaso.programista@gmail.com>
2024-01-31 13:09:26 +0100

    added repository DataJpaTest and moved db creation to currency-persistence-service

--- src/main/java/pl/kurs/exchange_api_micro/repository/ExchangeApiRepository.java
+++ src/main/java/pl/kurs/exchange_api_micro/repository/ExchangeApiRepository.java
@@ -1,6 +1,7 @@
 package pl.kurs.exchange_api_micro.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.kurs.exchange_api_micro.model.CurrencyRate;
 
 public interface ExchangeApiRepository extends JpaRepository<CurrencyRate, Integer> {
---
+++ src/test/java/pl/kurs/exchange_api_micro/repository/ExchangeApiRepositoryTest.java
@@ -0,0 +1,87 @@
+package pl.kurs.exchange_api_micro.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Repository;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import pl.kurs.exchange_api_micro.model.CurrencyRate;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@DataJpaTest
+@Testcontainers
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class ExchangeApiRepositoryTest {
+
+    @SpyBean
+    private ExchangeApiRepository repository;
+
+    private CurrencyRate dollar;
+    private CurrencyRate frank;
+
+
+    @Container
+    @ServiceConnection
+    private static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
+
+
+    @BeforeEach
+    public void setUp() {
+        dollar = CurrencyRate.builder()
+                .code("USD")
+                .currency("US Dollar")
+                .bid(BigDecimal.valueOf(3.99))
+                .ask(BigDecimal.valueOf(4.15))
+                .build();
+        frank = CurrencyRate.builder()
+                .code("CHF")
+                .currency("Frank")
+                .bid(BigDecimal.valueOf(4.49))
+                .ask(BigDecimal.valueOf(4.61))
+                .build();
+    }
+
+
+    @Test
+    public void connectionEstablished() {
+        assertTrue(postgres.isCreated());
+        assertTrue(postgres.isRunning());
+    }
+
+    @Test
+    public void testFindByCode_HappyPath_ResultsInCurrencyBeingReturned() {
+        CurrencyRate usd = repository.findByCode("USD");
+        CurrencyRate chf = repository.findByCode("CHF");
+
+        assertThat(usd).usingRecursiveComparison().isEqualTo(dollar);
+        assertThat(chf).usingRecursiveComparison().isEqualTo(frank);
+        verify(repository, times(2)).findByCode(anyString());
+    }
+
+    @Test
+    public void testFindByCode_CurrencyNotExists_ResultsInNullBeingReturned() {
+        CurrencyRate klp = repository.findByCode("KLP");
+
+        assertNull(klp);
+        verify(repository, times(1)).findByCode("KLP");
+    }
+}
